b3f3b03b81508d302eb629abb157e1de4da6a039
Roopali sahu <163496777+roopali111@users.noreply.github.com>
2024-06-06 21:23:30 +0530

    Add files via upload

---
+++ prgm1-5.js
@@ -0,0 +1,105 @@
+//                     1  Program to check number is odd or even
+
+// let arr = [1,2,3,4,5,8]
+// for(let i=0; i <=arr.length-1;i++){
+//     if(i%2===0){
+//         console.log(arr[i]);
+//     }
+  
+//    else {
+//     console.log("no even odd");
+   
+// }}
+
+//                        4  Program to Swap Two Variables values
+// let a =10;
+// let b = 20
+// let temp = '';
+// temp=a;
+// a=b;
+// b=temp
+
+// or
+
+// [a,b]=[b,a]
+// console.log(a);
+// console.log(b)
+
+//                            5  Find the largest number among three numbers
+// let num1 = 100;
+// let num2 = 700;
+// let num3 = 8590;
+
+
+// if(num1>num2 && num1>num3){
+//     console.log(num1);
+// }
+// else if(num2>num1 && num2>num3){
+//     console.log(num2);
+// }
+// else{
+//     console.log(num3);
+// }
+
+//                           3   Program to Find the Square Root of given Number
+
+// let x = 729;
+// let z= Math.sqrt(x)
+// console.log(z);
+
+// let x = 729;                       cube 
+// let z= Math.cbrt(x)
+// console.log(z);
+
+//                       10   Program to Check Prime Number
+
+// let  num = 123;
+// let count = 0
+
+// for(let i =0;i<=num;i++)
+// if(num%i==0){
+//     count++
+
+// }
+// if(count==2){
+//     console.log(`${num} is prime`);
+// }
+// else{
+//     console.log(`${num} is not prime`);
+
+// }
+
+//    9   Program to Check if a number is Positive, Negative or Zero
+
+// let a = 0
+
+
+// if(a>0 ){
+//     console.log(`${a} a is positive No`);
+// }
+// else if (a<0 ){
+//     console.log(`${a} a is negative  No`);
+// }
+// else{
+//     console.log(`${a} a is Zero`);
+// }
+
+// let b =25
+// let h = 25
+//                      11 find Area 
+// let Area = (b*h)/2
+// console.log(`the area of triange is ${Area}`);
+
+
+//                     6 convert kilometers to miles
+
+// let km = 10
+// let kmToMile = `${km}`*0.6213
+// console.log(kmToMile);
+
+//                  8  Program to Convert Celsius to Fahrenheit
+// let ces = 10
+// let far =(`${ces}`*(9/5)+32)
+// console.log(far);
+
+//                    12     Program to Display the Multiplication Table
